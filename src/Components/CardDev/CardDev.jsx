a7a1522445e2a725ed40249b159442f7163b1a53
igorsans <igor.santos0@hotmail.com>
2022-08-29 14:26:07 -0300

    :sparkles: feat: feito logica de alteração de card

--- src/Components/CardDev/CardDev.jsx
+++ src/Components/CardDev/CardDev.jsx
@@ -1,11 +1,11 @@
 
 
-const CardDev = ({img, nome, linkedin, github, texto, dev}) => {
+const CardDev = ({img, nome, linkedin, github, texto, cards}) => {
   return (
     <div>
         <img src={img}></img>
         <p>{nome}</p>
-        {dev == true ? <div>
+        {cards == true ? <div>
             <a target='_blank' href={linkedin}>linkedIn</a><br/>
             <a target='_blank' href={github}>GitHub</a>
         </div> : <p>{texto}</p>}
